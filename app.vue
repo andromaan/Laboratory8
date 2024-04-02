4710c8944b45c6c348a147d38f1fdc17ddc68c31
andromaan <andrii.lavreniuk@oa.edu.ua>
2024-04-02 15:32:30 +0300

    fix: app.vue again

--- app.vue
+++ app.vue
@@ -5,6 +5,6 @@
       <a href="/students" class="inline-flex items-center font-medium rounded-md text-lg px-5 py-1 bg-gray-50 dark:bg-gray-400 dark:bg-opacity-10 text-white dark:text-white ring-1 ring-inset ring-gray-500 dark:ring-gray-400 ring-opacity-25 dark:ring-opacity-25">Список студентів</a>
     </nav>
     <hr>
-    <nuxt-page />
+    <NuxtPage />
   </div>
 </template>
